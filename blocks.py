f64bf91e36480c84fbac0ae6efe5c1811031ba44
Danny Weitekamp <dannyweitekamp@gmail.com>
2026-06-11 18:23:13 -0400

    padding to Mapping str

--- blocks.py
+++ blocks.py
@@ -1,5 +1,11 @@
 from penumbra import PenumbraPredicate as PPred
 from cre import PredicateType, Var, AND, OR, Undef
+import cre
+
+def FactSet(facts):
+    fs = cre.FactSet()
+    for f in facts: fs.declare(f)
+    return fs
 
 # Static entity types
 Block = PredicateType("Block", {}, inherits_from=PPred)
@@ -21,150 +27,9 @@ Unstack       = PredicateType("Unstack",       {"block1": PPred, "block2": PPred
 PickupAndStack = PredicateType("PickupAndStack", {"block1": PPred, "table": PPred, "block2": PPred},    inherits_from=PPred)
 BuildTower    = PredicateType("BuildTower",    {"top": PPred, "middle": PPred, "table": PPred, "bottom": PPred}, inherits_from=PPred)
 
-# class PPred(PredicateType):
-#     ...
-
-# PPred : a, b, c        {a,b,c}
-# OtherClass : x, y, z   {x,y,z}
-# Clear :                {a,b,c,x,y,z}
-
-
-
-
-# class Clear(PPred, OtherClass):
-#     block : PPred
-
-
-# A representation similar to LISP Penumbra 
-
-# definitions = [
-
-rule1 = (AND(Pickup(id=(Var("block"), Var("table")), ts=Var("t1"), te=Var("t2"))) << 
-  AND(
-    Block(id=Var("block")),
-    Table(id=Var("table")),
-    Hand(id=Var("hand")),
-    Clear(id=(Var("block"),), ts=Var("t0"), te=Var("t3")),
-    Empty(id=(Var("hand"),), ts=Var("t4"), te=Var("t1")),
-    OnTable(id=(Var("block"), Var("table")), ts=Var("t5"), te=Var("t1")),
-    Holding(id=(Var("hand"), Var("block")), ts=Var("t2"), te=Var("t6")),
-
-    # Var("t2") == Var("t1"), Var("t0") <= Var("t1"), Var("t1") <= Var("t3"),
-    # Var("t1") < Var("t6"), Var("t4") < Var("t2"), Var("t5") < Var("t2")
- )
-)
-
-print(rule1)
-
-# print("----------------")
-
-rule2 = (AND(pickup := Var(Pickup)) << 
-  AND(
-    block := Var(Block),
-    table := Var(Table),
-    hand := Var(Hand),
-    clear := Var(Clear(block=block)),
-    empty := Var(Empty(hand=hand)),
-    ontable := Var(OnTable(block=block, table=table)),
-    holding := Var(Holding(hand=hand, object=table)),
-
-#     # pickup.ts+1 == pickup.te, # Pickup takes 1s
-
-#     # # Note: these two are implicit in previous repr 
-#     # empty.te == pickup.ts, # Empty ends when pickup starts
-#     # ontable.te == pickup.ts, # Ontable ends when pickup starts
-
-#     # # Only pickup while clear
-#     # clear.ts <= pickup.ts,   # t0 <= t1
-#     # pickup.ts <= clear.te,   # t1 <= t3
-
-#     # # Pickup begins before end of holding
-#     # pickup.ts < holding.te,  # t1 < t6
-
-#     # # Empty begins before pickup ends
-#     # empty.ts < pickup.te,    # t4 < t2
-
-#     # # OnTable begins before pickup ends
-#     # ontable.ts < pickup.te,  # t5 < t2
- )
-)
-
-# print(rule2)
-
-print("----------------")
-
-def before(a,b):
-    return
-
-pickup = +Pickup()
-print(pickup.ts)
-print(pickup.ts+1)
-print(pickup.ts+1 == pickup.te)
-
-print(type(pickup.ts+1 == pickup.te))
-
-print("vvvvvvvvvvvvv")
-
-block = +Block()
-clear = +Clear(block=block),
-
-print("-----")
-AND(
-  block := +Block(),
-  clear := +Clear(block=block),
-)
-print(block)
-print("-----")
-print(clear)
-
-print("^^^^^^^^^^^^")
-
-
-# fact = Block()
-# logic = +Block() 
-
-# fact.ts -> Value
-# fact.ts -> Variable
-
-# v = Var(Block)
-# v.ts -> Var
-
-# v.ts 
-
-def RuleDecorator(f):
-    print("Wrap")
-    # def func():
-    #     print("Call")
-    #     return x
-    z = f()
-
-    return z
-
-@RuleDecorator
-def rule1():
-    return (
-      AND(
-        block   := +Block(),
-        table   := +Table(),
-        hand    := +Hand(),
-
-      ) >> AND(+Pickup())
-    )
-
-
-def MyFunc(x,y):
-    print("call MyFunc")
-
-MyFunc(
-    x := print("make x"),
-    y := print("make y")
-)
-# moo()
-
-# Rule("Moo")(
 
 rule3 = ((pickup := +Pickup()) <<
-  AND(
+  (conds := AND(
     block   := +Block(),
     table   := +Table(),
     hand    := +Hand(),
@@ -191,11 +56,48 @@ rule3 = ((pickup := +Pickup()) <<
 
     # # OnTable begins before pickup ends
     # ontable.ts < pickup.te,  # t5 < t2
-  )
+  ))
 )
 
 print(rule3)
-print(rule3)
+
+
+# ---------------------------------------------------------------------------
+# Observations from blocks.lisp (setq observations* ...)
+# ---------------------------------------------------------------------------
+
+observations = FactSet([
+    A := Block(id='A', verity=True, ts=1, te=10),
+    B := Block(id='B', verity=True, ts=1, te=10),
+    C := Block(id='C', verity=True, ts=1, te=10),
+    D := Table(id='D', verity=True, ts=1, te=10),
+    H := Hand( id='H', verity=True, ts=1, te=10),
+    OnTable(id=('C','D'), block=C, table=D, verity=True, ts=1, te=10),
+    # --- before pickup(B,D) ---
+    Empty(  id=('H',),    hand=H,            verity=True, ts=1, te=2),
+    Clear(  id=('B',),    block=B,           verity=True, ts=1, te=8),
+    OnTable(id=('B','D'), block=B, table=D,  verity=True, ts=1, te=2),
+    # --- after pickup(B,D), before stack(B,C) ---
+    Holding(id=('H','B'), hand=H, object=B,  verity=True, ts=3, te=4),
+    Clear(  id=('C',),    block=C,           verity=True, ts=1, te=4),
+    # --- after stack(B,C) ---
+    On(     id=('B','C'), above=B, below=C,  verity=True, ts=5, te=10),
+    Empty(  id=('H',),    hand=H,            verity=True, ts=5, te=6),
+    Clear(  id=('A',),    block=A,           verity=True, ts=1, te=10),
+    OnTable(id=('A','D'), block=A, table=D,  verity=True, ts=1, te=6),
+    # --- after pickup(A,D), before stack(A,B) ---
+    Holding(id=('H','A'), hand=H, object=A,  verity=True, ts=7, te=8),
+    # --- after stack(A,B) ---
+    On(     id=('A','B'), above=A, below=B,  verity=True, ts=9, te=10),
+    Empty(  id=('H',),    hand=H,            verity=True, ts=9, te=10),
+])
+
+print(observations)
+
+itr = conds.get_partial_matches(observations)
+
+for match in itr:
+    print(match)
 
 
 
